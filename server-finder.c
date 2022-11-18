6cfe0dfcb5e6ed2c64a6ec24574adc56fc62e72b
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-11-18 19:25:24 -0300

    Server Finder v1

---
+++ server-finder.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+char enteroACaracter(int numero);
+
+int main() {
+    int Descriptor;
+    int tama_sin;    
+
+    char direccion[15];
+    printf("Ingrese su direccion de red: ");
+    scanf("%s", direccion);
+    system("clear");    
+    
+    int flag = 0;
+    int posicionHostID;
+
+    if ((Descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error en Socket()\n");
+        exit(-1);
+    }
+
+    for (int i = 0; i < sizeof(direccion); i++) {
+
+        if(flag > 2) {
+            posicionHostID = i;
+            direccion[posicionHostID] = '\0';
+            break;
+        }
+        
+        if(direccion[i] == '.') {
+            flag++;
+        };
+    }
+
+    direccion[posicionHostID] = '0';
+    direccion[posicionHostID + 1] = '\0';
+
+    printf("Dirección de Red %s\n", direccion);
+    printf("Buscando servidores...\n");
+
+    struct sockaddr_in servidor;
+    servidor.sin_family = AF_INET;
+    servidor.sin_port = htons(80);
+
+    for (int i = 1; i < 255; i++) {
+        if (i < 10) {
+            char c = enteroACaracter(i);
+            direccion[posicionHostID] = c;
+            
+        } else if (i < 100) {
+            char c1 = enteroACaracter(i / 10);
+            char c2 = enteroACaracter(i % 10);
+            direccion[posicionHostID] = c1;
+            direccion[posicionHostID + 1] = c2;
+            direccion[posicionHostID + 2] = '\0';
+        } else {
+            char c1 = enteroACaracter(i / 100);
+            char c2 = enteroACaracter(i / 10 % 10);
+            char c3 = enteroACaracter(i % 10);
+            direccion[posicionHostID] = c1;
+            direccion[posicionHostID + 1] = c2;
+            direccion[posicionHostID + 2] = c3;
+            direccion[posicionHostID + 3] = '\0';
+        }  
+
+        servidor.sin_addr.s_addr = inet_addr(direccion);
+        bzero(&(servidor.sin_zero), 8);
+
+        if (connect(Descriptor, (struct sockaddr *)&servidor, sizeof(servidor)) == 0) {
+                       
+            printf("Servidor encontrado IP: %s\n",direccion);            
+            close();
+        }
+    }    
+    
+}
+
+char enteroACaracter(int numero){
+    return numero + '0';
+}
